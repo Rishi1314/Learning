33c31c71d043cf0c6f302ff20d1295fa2bc8d0a4
Rishi1314 <rishirajprof@gmail.com>
2024-01-06 21:01:29 +0530

    Basics using just redux and react-redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useSelector,useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch=useDispatch();
+  const increment=()=>{
+    dispatch({type:'INC'})
+  }
+  const decrement=()=>{
+    dispatch({type:'DEC'})
+  }
+  const add=()=>{
+    dispatch({type:"ADD",payload:10})
+  }
+  const counter=useSelector((state)=>state.counter);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+      <h1>Counter</h1>
+      <h2>{counter}</h2>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={add}>Add</button>
     </div>
   );
 }
